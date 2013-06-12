1e922a27a36e429af584c8f90011c74454f0d890
Ryan Newton <rrnewton@gmail.com>
2013-06-12 16:30:15 -0400

    Progress on array fusion layer but running into a typing problem

--- Examples/fir/Fir_idealized.hs
+++ Examples/fir/Fir_idealized.hs
@@ -1,10 +1,12 @@
-
+{-# LANGUAGE ExistentialQuantification, ScopedTypeVariables #-}
 
 import qualified Language.StreamIt as S
+import Language.StreamIt -- (Exp, Filter, Var)
 -- Functional layer:
 -- import qualified Language.StreamIt.Fun as F
 import qualified Prelude as P
-import Prelude ((*),($), error, Int, Float, Num)
+import Prelude ((+),(*),($), error, return,
+                Int, Float, Num)
 
 {-
 
@@ -42,7 +44,7 @@ import Prelude ((*),($), error, Int, Float, Num)
 fir :: Int -> SArray Float -> S.Filter Float Float ()
 fir n weights =
   filter $ \ strm loopK -> 
-    let window = take n strm in
+    let window = take (S.constant n) strm in
     sum (zipWith (*) weights window) <:>
      loopK (tail strm)
 
@@ -53,27 +55,53 @@ fir n weights =
 data Stream a = Stream Int
   -- ^ Right now it's nothing but a cursor that starts at zero.
 
--- | A StreamIt (staged) array. 
-data SArray a =
-  SArray
-  {
--- FINISHME:
+-- | A StreamIt (staged) array.
 -- 
 --  Here we use a push/pull array hybrid where the consumer chooses which
 --  representation to use.
 -- 
 --  (This combines push-arrays with the generalized-stream-fusion style multi-rep
 --  bundling.)
+data SArray a =
+  SArray
+  { pushrep :: PushArray a
+  , pullrep :: PullArray a 
+  }  
+
+data PushArray a = forall b c . (S.Elt b, S.Elt c) =>
+                   PushArray ((Exp Int -> Exp a -> Filter b c ()) -> Filter b c ())
+type PullArray a = (Exp Int -> Exp a)
+
+-- | Turn a manifest array bound to a variable into an array that can be used with
+-- the combinators.
+namedArr :: Var (SArray a) -> SArray a
+namedArr vr =
+  SArray
+  { pushrep = PushArray $ \ rcvr -> error "pushrep"
+  , pullrep = \ ix -> error "pullrep -- need array deref"
+                      -- (vr S.!)
   }
-
-data PushArray a
-data PullArray a
-
   
 --------------------------------------------------------------------------------
 
-take :: Int -> Stream a -> SArray a
-take = error "take"
+take :: Elt a => Exp Int -> Stream a -> SArray a
+take n (Stream cursor) =
+  SArray
+  { pullrep = \ ix -> error "FIXME - peek " -- S.peek (constant cursor + ix)
+  , pushrep = loopPusher n (\ ix -> peek (S.constant cursor + ref ix))
+  }
+
+-- | Capture the common pattern of sequentially pushing all elements of the array
+-- with a for loop.
+loopPusher :: Elt a => Exp Int -> (Var Int -> Exp a) -> PushArray a 
+loopPusher len body = 
+  PushArray $ \ rcvr -> do
+           -- Here we loop through and do all the peeks, pushing them to the consumer:
+           ix <- int
+           for_ (ix <== 0, ref ix <. len, ix <== ref ix + 2) $ do 
+              rcvr (ref ix) (body ix)
+           return ()
+
 
 scons :: S.Exp a -> Stream a -> Stream a
 scons = error "scons"
