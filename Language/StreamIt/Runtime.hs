df3230e4e125a46ecc512ef5c1fa2291e282ecdd
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2013-06-20 13:51:23 -0400

    pushing some unstaged changes enabling alternate staged compilation methods

--- Language/StreamIt/Runtime.hs
+++ Language/StreamIt/Runtime.hs
@@ -12,13 +12,23 @@ import System.Posix.Files
 import System.Posix.Process
 
 import Language.StreamIt.Backend
+import Language.StreamIt.Compile
 
-runTbb :: L.ByteString -> IO ()
-runTbb = undefined
+-- Default runtime method
+run :: Target -> Embedding -> IO ()
+run t e = case t of
+  StreamIt -> case e of 
+    EmbBinary bs -> runStrEmbedded bs
+    DynLink bs -> runStrDynLink bs
+    DynLoad bs -> runStrDynLoad bs
+  TBB -> case e of
+    EmbBinary bs -> runTBBEmbedded bs
+    DynLink bs -> runTBBDynLink bs
+    DynLoad bs -> runTBBDynLoad bs
 
--- | Runs a StreamIt executable at run-time
-runStreamIt :: L.ByteString -> IO ()
-runStreamIt bs = do
+-- | Runs a StreamIt executable embedded as a lazy bytestring
+runStrEmbedded :: L.ByteString -> IO ()
+runStrEmbedded bs = do
   (tf, th) <- openBinaryTempFile "." "streamit"
   L.hPut th (decompress bs)
   setFileMode tf ownerExecuteMode
@@ -28,8 +38,20 @@ runStreamIt bs = do
   getProcessStatus True False pid
   removeFile tf
 
+-- | Runs a StreamIt executable embedded as a lazy bytestring
+runStrDynLink :: L.ByteString -> IO ()
+runStrDynLink s = undefined
+  
+-- | Runs a StreamIt executable embedded as a lazy bytestring
+runStrDynLoad :: L.ByteString -> IO ()
+runStrDynLoad s = undefined
+  
+-- TBB runtime functions
+runTBBEmbedded :: L.ByteString -> IO ()
+runTBBEmbedded = undefined
 
-run :: Target -> L.ByteString -> IO ()
-run target bs = case target of
-  StreamIt -> runStreamIt bs
-  TBB -> runTbb bs
+runTBBDynLink :: L.ByteString -> IO ()
+runTBBDynLink = undefined
+
+runTBBDynLoad :: L.ByteString -> IO ()
+runTBBDynLoad = undefined
